3df70581163bee43dfd22f72c71b905633b7e964
syed abdul hakim <s.abdulhakim10@gmail.com>
2022-11-07 11:16:52 +0600

    create products API to load data from mongodb

--- index.js
+++ index.js
@@ -16,6 +16,23 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.47nvmfs.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try{
+        const productCollection = client.db('emaJohn').collection('products');
+
+        
+        app.get('/products', async(req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(err => console.error(err));
 
 
 app.get('/', (req, res) => {
