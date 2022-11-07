a98ad5e7165e94b539ad57eb51a17857a2c63ea0
syed abdul hakim <s.abdulhakim10@gmail.com>
2022-11-07 18:24:52 +0600

    Use Post to load some products using keys

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -20,13 +20,25 @@ async function run() {
     try{
         const productCollection = client.db('emaJohn').collection('products');
 
-        
+        // pagination practice
         app.get('/products', async(req, res) => {
+            const page = parseInt(req.query.page);
+            const size = parseInt(req.query.size);
+            console.log(page, size)
             const query = {};
             const cursor = productCollection.find(query);
-            const products = await cursor.toArray();
+            const products = await cursor.skip(page*size).limit(size).toArray();
             const count = await productCollection.estimatedDocumentCount();
             res.send({count, products});
+        })
+
+        app.post('/productsByIds', async(req, res) => {
+            const ids = req.body;
+            const objectIds = ids.map(id => ObjectId(id));
+            const query = {_id: {$in: objectIds}};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
         })
     }
     finally{
